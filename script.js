66f65a34325d2a2306cc570917ee3f5b3c647d35
akitesan <cycle.rak.67@gmail.com>
2025-08-03 00:28:45 +0900

    add svg file

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ createApp({
     data() {
         return {
             svgMap: '',
-            visited: JSON.parse(localStorage.getItem('visited') || '{}')
+            visited: JSON.parse(localStorage.getItem('visited') || {})
         };
     },
     mounted() {
-        fetch('japan.svg')
+        fetch('map-full.svg')
             .then(res => res.text())
             .then(svg => {
                 this.svgMap = svg;
@@ -17,20 +17,21 @@ createApp({
     },
     methods: {
         togglePrefecture(event) {
-            if (!event.target.id.startsWith('JP-')) return;
-            const id = event.target.id;
-            this.visited[id] = !this.visited[id];
+            const g = event.target.closest('g[data-code]');
+            if (!g) return;
+            const code = g.getAttribute('data-code');
+            this.visited[code] = !this.visited[code];
             localStorage.setItem('visited', JSON.stringify(this.visited));
-            event.target.classList.toggle('visited');
+            g.classList.toggle('visited');
         },
         applyVisitedStyles() {
             this.$nextTick(() => {
-                for (const id in this.visited) {
-                    if (this.visited[id]) {
-                        const el = document.getElementById(id);
-                        if (el) el.classList.add('visited');
+                Object.keys(this.visited).forEach(code => {
+                    if (this.visited[code]) {
+                        const g = document.querySelector(`g[data-code="${code}"]`);
+                        if (g) g.classList.add('visited');
                     }
-                }
+                });
             });
         }
     }
